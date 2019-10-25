77cad4d2efd49c03d2c0e4782b3f7a6de8ec4f8b
LongoMichele <michele.longo@student.unife.it>
2019-10-26 00:56:03 +0200

    Added a demo for the quote of the day (from a programmer).

--- demos/weatherForecast.php
+++ demos/weatherForecast.php
@@ -1,5 +1,5 @@
 <?php
-	if ($fh = fopen('../.weatherUserID', 'r')) {
+	if ($fh = fopen('../res/.weatherUserID', 'r')) {
 		$userID = fgets($fh);
 		fclose($fh);
 	}
@@ -18,23 +18,22 @@
 	$curdate = substr($json->list[0]->dt_txt, 8, 2);
 	$forecast = array();
 	$day = array();
-	for($i = 0; $i < count($json->list); $i++) {
-		if($curdate != substr($json->list[$i]->dt_txt, 8, 2)) {
-			$curdate = substr($json->list[$i]->dt_txt, 8, 2);
-			array_push($forecast, $day);
-			$day = array();
-		}
-		$time = substr($json->list[$i]->dt_txt, 11, 5);
-		$humidity = $json->list[$i]->main->humidity . '%';
-		$temp = $json->list[$i]->main->temp . '&deg;C';
-		$maxtemp = $json->list[$i]->main->temp_max . '&deg;C';
-		$mintemp = $json->list[$i]->main->temp_min . '&deg;C';
-		$weather = $json->list[$i]->weather[0]->description;
-		$weather = strtoupper(substr($weather, 0, 1)) . substr($weather, 1, strlen($weather)-1);
-		$icon = 'icons/' . $imgDim . '/' . $json->list[$i]->weather[0]->icon . '.png';
-
-		$hour = ['weather' => $weather, 'general' => $json->list[$i]->weather[0]->main, 'icon' => $icon, 'time' => $time, 'humidity' => $humidity, 'temp' => $temp, 'maxtemp' => $maxtemp, 'mintemp' => $mintemp];
-		array_push($day, $hour);
+	foreach($json->list as $hour) {
+		if($curdate != substr($hour->dt_txt, 8, 2)) {
+				$curdate = substr($hour->dt_txt, 8, 2);
+				array_push($forecast, $day);
+				$day = array();
+			}
+			$time = substr($hour->dt_txt, 11, 5);
+			$humidity = $hour->main->humidity . '%';
+			$temp = $hour->main->temp . '&deg;C';
+			$maxtemp = $hour->main->temp_max . '&deg;C';
+			$mintemp = $hour->main->temp_min . '&deg;C';
+			$weather = $hour->weather[0]->description;
+			$weather = strtoupper(substr($weather, 0, 1)) . substr($weather, 1, strlen($weather)-1);
+			$icon = 'icons/' . $imgDim . '/' . $hour->weather[0]->icon . '.png';
+	
+			array_push($day, ['weather' => $weather, 'general' => $hour->weather[0]->main, 'icon' => $icon, 'time' => $time, 'humidity' => $humidity, 'temp' => $temp, 'maxtemp' => $maxtemp, 'mintemp' => $mintemp]);
 	}
 
 	echo '<h1>Showing only 1 report</h1><br>';
