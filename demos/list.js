a954cc7eb3d7047110b7831fa10b92fd36c0227c
LongoMichele <michele.longo@student.unife.it>
2019-10-26 23:25:30 +0200

    100% completed quick links.

--- demos/list.js
+++ demos/list.js
@@ -1,4 +1,6 @@
 function showChild(id) {
+	document.getElementById(id).classList.remove('folderClosed');
+	document.getElementById(id).classList.add('folderOpened');
 	var childs = document.getElementById(id).children;
 	for (var i = 0; i < childs.length; i++) {
 		childs[i].style.display = 'block';
